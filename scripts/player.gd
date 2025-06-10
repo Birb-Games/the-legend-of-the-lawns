012e3b69c555db80bbb82976bbb4a7e5d7fa805e
JL <74738871+JLi69@users.noreply.github.com>
2025-06-10 12:26:49 -0500

    added ability to pull lawnmower

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var lawnmower: RigidBody2D
+
 var speed: int = 80
 
 func _physics_process(_delta: float):
@@ -19,5 +21,11 @@ func _physics_process(_delta: float):
 	if velocity.length() > 0.0:
 		velocity /= velocity.length()
 	velocity *= speed
+	
+	# Pull lawnmower with player
+	if Input.is_action_pressed("pull") and $Pull.can_pull:
+		lawnmower.linear_velocity = velocity
+	elif Input.is_action_just_released("pull") and $Pull.can_pull:
+		lawnmower.linear_velocity = Vector2.ZERO
 
 	move_and_slide()
