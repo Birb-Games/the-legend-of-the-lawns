985e7f8fd53f0d3fb36396cec5522ef0cbf42e96
GldeA <glde.abbott@gmail.com>
2025-06-11 15:38:58 -0500

    Made the lawnmower only pullable in the opposite direction (can no longer drag to side or diagonal)

--- scripts/player.gd
+++ scripts/player.gd
@@ -23,9 +23,13 @@ func _physics_process(_delta: float):
 	velocity *= speed
 	
 	# Pull lawnmower with player
-	if Input.is_action_pressed("pull") and $Pull.can_pull:
+	var comparison_vector: Vector2 = abs(velocity.normalized() + Vector2.from_angle(get_angle_to(lawnmower.position)))
+	var same_direction: bool = comparison_vector.x <= 0.1 or comparison_vector.y <= 0.1
+		# Compare the velocity direction with the angle to the lawnmower's position, if moving directly away from mower, it can be pulled
+		
+	if Input.is_action_pressed("pull") and $Pull.can_pull and same_direction:
 		lawnmower.linear_velocity = velocity
-	elif Input.is_action_just_released("pull") and $Pull.can_pull:
+	elif (Input.is_action_just_released("pull") and $Pull.can_pull) or !same_direction:
 		lawnmower.linear_velocity = Vector2.ZERO
 
 	move_and_slide()
