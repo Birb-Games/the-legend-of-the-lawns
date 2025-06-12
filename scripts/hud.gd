ddc79cf3faffd65c0b4bb0ad704367f156908b07
GldeA <glde.abbott@gmail.com>
2025-06-12 17:22:11 -0500

    Finished progress bar implementation and added percentage indicator

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -10,3 +10,4 @@ func update_info_text(text: String) -> void:
 # updates progress bar based on the given percent (0.0 to 1.0)
 func update_progress_bar(percent: float) -> void:
 	$Control/ProgressBar.size.x = percent * $Control/ProgressBackground.size.x
+	$Control/ProgressBarPercent.text = str(int(percent * 100)) + "%"
