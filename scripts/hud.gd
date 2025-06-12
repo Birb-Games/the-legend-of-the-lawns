90ee9a258da489dfb35cbfe34c2089754395ec03
GldeA <glde.abbott@gmail.com>
2025-06-12 16:47:37 -0500

    Added unimplemented progress bar

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -6,3 +6,7 @@ func _ready() -> void:
 # used for pop up messages to provide information to the player
 func update_info_text(text: String) -> void:
 	$Control/InfoText.text = text
+
+# updates progress bar based on the given percent (0.0 to 1.0)
+func update_progress_bar(percent: float) -> void:
+	$Control/ProgressBar.size.x = percent * $Control/ProgressBackground.size.x
