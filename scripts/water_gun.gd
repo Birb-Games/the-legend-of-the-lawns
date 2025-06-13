d27d043f08443fd3fbae82d8e39d39c574b4a3a9
GldeA <glde.abbott@gmail.com>
2025-06-13 11:14:02 -0500

    Made water gun hidden in neighborhood

--- scripts/water_gun.gd
+++ scripts/water_gun.gd
@@ -7,19 +7,25 @@ var radius = position.x
 var size = scale.x
 
 func _process(_delta: float) -> void:
-	position = (get_global_mouse_position() - get_parent().position).normalized() * radius
-	rotation = (get_global_mouse_position() - get_parent().position).normalized().angle()
-	if abs(rotation) < PI / 2:
-		scale.y = size
+	if $/root/Main.lawn_loaded:
+		visible = true
+		position = (get_global_mouse_position() - get_parent().position).normalized() * radius
+		rotation = (get_global_mouse_position() - get_parent().position).normalized().angle()
+		if abs(rotation) < PI / 2:
+			scale.y = size
+		else:
+			scale.y = -size
+		
+		if Input.is_action_just_pressed("shoot"):
+			particles.emitting = true
+			raycast.enabled = true
+		if Input.is_action_pressed("shoot"):
+			if raycast.is_colliding():
+				print("Water gun is hitting: ", raycast.get_collider().name)
+		else:
+			get_child(0).emitting = false
+			raycast.enabled = false
 	else:
-		scale.y = -size
-	
-	if Input.is_action_just_pressed("shoot"):
-		particles.emitting = true
-		raycast.enabled = true
-	if Input.is_action_pressed("shoot"):
-		if raycast.is_colliding():
-			print("Water gun is hitting: ", raycast.get_collider().name)
-	else:
-		get_child(0).emitting = false
+		particles.emitting = false
 		raycast.enabled = false
+		visible = false
