d9dfa753304b648e5b02f2e5ccf5c8ed73c91170
GldeA <glde.abbott@gmail.com>
2025-06-19 20:25:51 -0500

    Finished hedge trimming (setting hedges to the correct types based on neighbors)

--- scripts/basic_lawn_generator.gd
+++ scripts/basic_lawn_generator.gd
@@ -9,9 +9,7 @@ func _ready() -> void:
 func generate_hedge(centerpoint: Vector2i) -> void:
 	# Create rectangle
 	var hedge: TileMapPattern = TileMapPattern.new()
-	var size: Vector2i = Vector2i(randi_range(1, 5), randi_range(2, 5))
-	if size.x == 1:
-		return #chance of no hedge at all
+	var size: Vector2i = Vector2i(randi_range(1, 5), randi_range(1, 5))
 	for i in range(size.x):
 		for j in range(size.y):
 			hedge.set_cell(Vector2i(i, j), 0, Vector2i(5, 1), 0)
@@ -22,83 +20,132 @@ func generate_hedge(centerpoint: Vector2i) -> void:
 # Give hedges their proper edges
 func prune_hedges() -> void:
 	for tile in get_used_cells():
-		if get_cell_atlas_coords(tile).x <= 6 and get_cell_atlas_coords(tile).x >= 3 and get_cell_atlas_coords(tile).y == 1:
+		if is_hedge(get_cell_atlas_coords(tile)):
 			# Check which of the neighbors is a hedge
 			var neighbors: Array = [false, false, false, false, false, false, false, false]
-			neighbors[0] = (get_cell_atlas_coords(tile + Vector2i(0, -1)).x <= 6 and get_cell_atlas_coords(tile + Vector2i(0, -1)).x >= 3) and get_cell_atlas_coords(tile + Vector2i(0, -1)).y == 1 # Up
-			neighbors[1] = (get_cell_atlas_coords(tile + Vector2i(0, 1)).x <= 6 and get_cell_atlas_coords(tile + Vector2i(0, 1)).x >= 3) and get_cell_atlas_coords(tile + Vector2i(0, 1)).y == 1   # Down
-			neighbors[2] = (get_cell_atlas_coords(tile + Vector2i(-1, 0)).x <= 6 and get_cell_atlas_coords(tile + Vector2i(-1, 0)).x >= 3) and get_cell_atlas_coords(tile + Vector2i(-1, 0)).y == 1 # Left
-			neighbors[3] = (get_cell_atlas_coords(tile + Vector2i(1, 0)).x <= 6 and get_cell_atlas_coords(tile + Vector2i(1, 0)).x >= 3) and get_cell_atlas_coords(tile + Vector2i(1, 0)).y == 1  # Right
-			neighbors[4] = (get_cell_atlas_coords(tile + Vector2i(-1, -1)).x <= 6 and get_cell_atlas_coords(tile + Vector2i(-1, -1)).x >= 3) and get_cell_atlas_coords(tile + Vector2i(-1, -1)).y == 1 # Top Left
-			neighbors[5] = (get_cell_atlas_coords(tile + Vector2i(1, -1)).x <= 6 and get_cell_atlas_coords(tile + Vector2i(1, -1)).x >= 3) and get_cell_atlas_coords(tile + Vector2i(1, -1)).y == 1  # Top Right
-			neighbors[6] = (get_cell_atlas_coords(tile + Vector2i(-1, 1)).x <= 6 and get_cell_atlas_coords(tile + Vector2i(-1, 1)).x >= 3) and get_cell_atlas_coords(tile + Vector2i(-1, 1)).y == 1  # Bottom Left
-			neighbors[7] = (get_cell_atlas_coords(tile + Vector2i(1, 1)).x <= 6 and get_cell_atlas_coords(tile + Vector2i(1, 1)).x >= 3) and get_cell_atlas_coords(tile + Vector2i(1, 1)).y == 1   # Bottom Right
+			neighbors[0] = is_hedge(get_cell_atlas_coords(tile + Vector2i(0, -1))) # Up
+			neighbors[1] = is_hedge(get_cell_atlas_coords(tile + Vector2i(0, 1)))   # Down
+			neighbors[2] = is_hedge(get_cell_atlas_coords(tile + Vector2i(-1, 0))) # Left
+			neighbors[3] = is_hedge(get_cell_atlas_coords(tile + Vector2i(1, 0)))  # Right
+			neighbors[4] = is_hedge(get_cell_atlas_coords(tile + Vector2i(-1, -1))) # Top Left
+			neighbors[5] = is_hedge(get_cell_atlas_coords(tile + Vector2i(1, -1)))  # Top Right
+			neighbors[6] = is_hedge(get_cell_atlas_coords(tile + Vector2i(-1, 1)))  # Bottom Left
+			neighbors[7] = is_hedge(get_cell_atlas_coords(tile + Vector2i(1, 1)))   # Bottom Right
 
-			var neighbor_count: int = 0
-			for i in neighbors:
-				neighbor_count += int(i)
-			
-			if neighbor_count == 0 or neighbor_count == 1 or neighbor_count == 2:
-				printerr("Invalid hedge placement at ", tile)
-			elif neighbor_count == 3:
-				if neighbors[4]:
-					set_cell(tile, 0, Vector2i(3, 1), 3)
-				elif neighbors[5]:
-					set_cell(tile, 0, Vector2i(3, 1), 2)
-				elif neighbors[6]:
-					set_cell(tile, 0, Vector2i(3, 1), 1)
-				elif neighbors[7]:
-					set_cell(tile, 0, Vector2i(3, 1), 0)
-			elif neighbor_count == 4:
-				if neighbors[0] and neighbors[2]:
-					set_cell(tile, 0, Vector2i(3, 1), 3)
-				elif neighbors[0] and neighbors[3]:
-					set_cell(tile, 0, Vector2i(3, 1), 2)
-				elif neighbors[1] and neighbors[2]:
-					set_cell(tile, 0, Vector2i(3, 1), 1)
-				elif neighbors[1] and neighbors[3]:
-					set_cell(tile, 0, Vector2i(3, 1), 0)
-			elif neighbor_count == 5:
-				if !neighbors[0] and neighbors[1] and neighbors[2] and neighbors[3]:
-					set_cell(tile, 0, Vector2i(4, 1), 0)
-				elif !neighbors[1] and neighbors[0] and neighbors[2] and neighbors[3]:
-					set_cell(tile, 0, Vector2i(4, 1), 1)
-				elif !neighbors[2] and neighbors[0] and neighbors[1] and neighbors[3]:
-					set_cell(tile, 0, Vector2i(4, 1), 2)
-				elif !neighbors[3] and neighbors[0] and neighbors[1] and neighbors[2]:
-					set_cell(tile, 0, Vector2i(4, 1), 3)
-				elif !neighbors[4]:
-					set_cell(tile, 0, Vector2i(3, 1), 0)
-				elif !neighbors[5]:
-					set_cell(tile, 0, Vector2i(3, 1), 1)
-				elif !neighbors[6]:
-					set_cell(tile, 0, Vector2i(3, 1), 2)
-				elif !neighbors[7]:
-					set_cell(tile, 0, Vector2i(3, 1), 3)
-			elif neighbor_count == 6:
-				if !neighbors[0]:
-					set_cell(tile, 0, Vector2i(4, 1), 0)
-				elif !neighbors[1]:
-					set_cell(tile, 0, Vector2i(4, 1), 1)
-				elif !neighbors[2]:
-					set_cell(tile, 0, Vector2i(4, 1), 2)
-				elif !neighbors[3]:
-					set_cell(tile, 0, Vector2i(4, 1), 3)
-			elif neighbor_count == 7:
-				if !neighbors[0]:
-					set_cell(tile, 0, Vector2i(4, 1), 0)
-				elif !neighbors[1]:
-					set_cell(tile, 0, Vector2i(4, 1), 1)
-				elif !neighbors[2]:
-					set_cell(tile, 0, Vector2i(4, 1), 2)
-				elif !neighbors[3]:
-					set_cell(tile, 0, Vector2i(4, 1), 3)
-				elif !neighbors[4]:
-					set_cell(tile, 0, Vector2i(6, 1), 0)
-				elif !neighbors[5]:
-					set_cell(tile, 0, Vector2i(6, 1), 1)
-				elif !neighbors[6]:
-					set_cell(tile, 0, Vector2i(6, 1), 2)
-				elif !neighbors[7]:
-					set_cell(tile, 0, Vector2i(6, 1), 3)
+			# Decide which hedge tile based on neighbors
+			# https://cdn.discordapp.com/attachments/1380235298599997480/1385342556291727601/hedges.png?ex=6855b84b&is=685466cb&hm=1ce057dfcd703d0d262e3a652fc8465ab4b266b588788eb5d955de270a6d4897&
+			match neighbors:
+				[false, false, false, false, _, _, _, _]:
+					set_cell(tile, 0, Vector2i(1, 2), 0)
 				
+				[true, false, false, false, _, _, _, _]:
+					set_cell(tile, 0, Vector2i(2, 2), 1)
+				[false, true, false, false, _, _, _, _]:
+					set_cell(tile, 0, Vector2i(2, 2), 0)
+				[false, false, true, false, _, _, _, _]:
+					set_cell(tile, 0, Vector2i(2, 2), 3)
+				[false, false, false, true, _, _, _, _]:
+					set_cell(tile, 0, Vector2i(2, 2), 2)
+				
+				[false, true, false, true, _, _, _, false]:
+					set_cell(tile, 0, Vector2i(3, 2), 0)
+				[false, true, true, false, _, _, false, _]:
+					set_cell(tile, 0, Vector2i(3, 2), 1)
+				[true, false, false, true, _, false, _, _]:
+					set_cell(tile, 0, Vector2i(3, 2), 2)
+				[true, false, true, false, false, _, _, _]:
+					set_cell(tile, 0, Vector2i(3, 2), 3)
+
+				[false, true, false, true, _, _, _, true]:
+					set_cell(tile, 0, Vector2i(3, 1), 0)
+				[false, true, true, false, _, _, true, _]:
+					set_cell(tile, 0, Vector2i(3, 1), 1)
+				[true, false, false, true, _, true, _, _]:
+					set_cell(tile, 0, Vector2i(3, 1), 2)
+				[true, false, true, false, true, _, _, _]:
+					set_cell(tile, 0, Vector2i(3, 1), 3)
+				
+				[true, true, false, false, _, _, _, _]:
+					set_cell(tile, 0, Vector2i(4, 2), 1)
+				[false, false, true, true, _, _, _, _]:
+					set_cell(tile, 0, Vector2i(4, 2), 0)
+				
+				[false, true, true, true, _, _, false, false]:
+					set_cell(tile, 0, Vector2i(5, 2), 0)
+				[true, false, true, true, false, false, _, _]:
+					set_cell(tile, 0, Vector2i(5, 2), 1)
+				[true, true, false, true, _, false, _, false]:
+					set_cell(tile, 0, Vector2i(5, 2), 2)
+				[true, true, true, false, false, _, false, _]:
+					set_cell(tile, 0, Vector2i(5, 2), 3)
+
+				[false, true, true, true, _, _, false, true]:
+					set_cell(tile, 0, Vector2i(6, 2), 0)
+				[false, true, true, true, _, _, true, false]:
+					set_cell(tile, 0, Vector2i(6, 2), 1)
+				[true, false, true, true, false, true, _, _]:
+					set_cell(tile, 0, Vector2i(6, 2), 2)
+				[true, false, true, true, true, false, _, _]:
+					set_cell(tile, 0, Vector2i(6, 2), 3)
+				[true, true, false, true, _, false, _, true]:
+					set_cell(tile, 0, Vector2i(6, 2), 4)
+				[true, true, false, true, _, true, _, false]:
+					set_cell(tile, 0, Vector2i(6, 2), 6)
+				[true, true, true, false, false, _, true, _]:
+					set_cell(tile, 0, Vector2i(6, 2), 5)
+				[true, true, true, false, true, _, false, _]:
+					set_cell(tile, 0, Vector2i(6, 2), 7)
+				
+				[false, true, true, true, _, _, true, true]:
+					set_cell(tile, 0, Vector2i(4, 1), 0)
+				[true, false, true, true, true, true, _, _]:
+					set_cell(tile, 0, Vector2i(4, 1), 1)
+				[true, true, false, true, _, true, _, true]:
+					set_cell(tile, 0, Vector2i(4, 1), 2)
+				[true, true, true, false, true, _, true, _]:
+					set_cell(tile, 0, Vector2i(4, 1), 3)
+
+				[true, true, true, true, false, false, false, false]:
+					set_cell(tile, 0, Vector2i(7, 2), 0)
+				
+				[true, true, true, true, true, false, false, false]:
+					set_cell(tile, 0, Vector2i(0, 3), 3)
+				[true, true, true, true, false, true, false, false]:
+					set_cell(tile, 0, Vector2i(0, 3), 2)
+				[true, true, true, true, false, false, true, false]:
+					set_cell(tile, 0, Vector2i(0, 3), 1)
+				[true, true, true, true, false, false, false, true]:
+					set_cell(tile, 0, Vector2i(0, 3), 0)
+				
+				[true, true, true, true, true, true, false, false]:
+					set_cell(tile, 0, Vector2i(1, 3), 1)
+				[true, true, true, true, false, false, true, true]:
+					set_cell(tile, 0, Vector2i(1, 3), 0)
+				[true, true, true, true, true, false, true, false]:
+					set_cell(tile, 0, Vector2i(1, 3), 3)
+				[true, true, true, true, false, true, false, true]:
+					set_cell(tile, 0, Vector2i(1, 3), 2)
+
+				[true, true, true, true, true, false, false, true]:
+					set_cell(tile, 0, Vector2i(2, 3), 0)
+				[true, true, true, true, false, true, true, false]:
+					set_cell(tile, 0, Vector2i(2, 3), 1)
+				
+				[true, true, true, true, false, true, true, true]:
+					set_cell(tile, 0, Vector2i(6, 1), 0)
+				[true, true, true, true, true, false, true, true]:
+					set_cell(tile, 0, Vector2i(6, 1), 1)
+				[true, true, true, true, true, true, false, true]:
+					set_cell(tile, 0, Vector2i(6, 1), 2)
+				[true, true, true, true, true, true, true, false]:
+					set_cell(tile, 0, Vector2i(6, 1), 3)
+
+				[true, true, true, true, true, true, true, true]:
+					pass
+
+				_:
+					printerr("Invalid hedge placement at ", tile)
+
+func is_hedge(atlas_coords: Vector2i) -> bool:
+	# Check if the tile is a hedge
+	return (atlas_coords.y == 1 and atlas_coords.x >= 3 and atlas_coords.x <= 6) or (atlas_coords.y == 2 and atlas_coords.x >= 1 and atlas_coords.x <= 7) or (atlas_coords.y == 3 and atlas_coords.x >= 0 and atlas_coords.x <= 2)
