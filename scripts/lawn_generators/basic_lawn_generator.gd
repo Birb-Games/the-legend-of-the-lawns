4e6767f6e14ae1268a23184809b7fd9a86475216
gldeA <glde.abbott@gmail.com>
2025-06-22 12:17:22 -0500

    Made hedges only generate on uncut grass

--- scripts/lawn_generators/basic_lawn_generator.gd
+++ scripts/lawn_generators/basic_lawn_generator.gd
@@ -12,15 +12,12 @@ func generate_hedge(generation_point: Node2D) -> void:
 	if randf() > generation_point.generation_chance:
 		return
 	
-	# Create rectangle
-	var hedge: TileMapPattern = TileMapPattern.new()
 	var size: Vector2i = Vector2i(randi_range(generation_point.min_size.x, generation_point.max_size.x), randi_range(generation_point.min_size.y, generation_point.max_size.y))
 	for i in range(size.x):
 		for j in range(size.y):
-			hedge.set_cell(Vector2i(i, j), 0, Vector2i(5, 1), 0)
-
-	# Place in lawn
-	set_pattern(Vector2i(generation_point.position / 16) - (hedge.get_size() / 2), hedge)
+			var current_tile: Vector2i = Vector2i(generation_point.position / 16) - (size / 2) + Vector2i(i, j)
+			if get_cell_atlas_coords(current_tile) == Vector2i(1, 0):	
+				set_cell(current_tile, 0, Vector2i(5, 1), 0)
 
 # Give hedges their proper edges
 func prune_hedges() -> void:
