ee01a238dc7caddbf7b90ec6af8df572d6089ea9
JL <74738871+JLi69@users.noreply.github.com>
2025-07-01 13:25:50 -0500

    cleaned up flower code - flower enemies now inherit from the FlowerEnemy class

--- scripts/enemies/blueflower.gd
+++ scripts/enemies/blueflower.gd
@@ -1,89 +1,32 @@
-extends Area2D
-
-@onready var player: Player = $/root/Main/Player
+extends FlowerEnemy
 
 @export var thorn_bullet: PackedScene
 @export var spore_bullet: PackedScene
-
-const MAX_HEALTH: int = 6
-var health: int = MAX_HEALTH
-const SHOOT_COOLDOWN: float = 6.0
-var shoot_timer: float = 0.0
-const SHOOT_RANGE: float = 132.0
-const BULLET_SPEED: float = 70.0
-var inside_lawnmower: bool = false
-const LAWNMOWER_DAMAGE_COOLDOWN: float = 1.0
-var lawnmower_damage_timer: float = 0.0
-const STUN_AMT: float = 12.0
-var stun_timer: float = 0.0
-
-func _ready() -> void:
-	$StunParticles.hide()
-	$StunParticles.emitting = false
 
 func rand_bullet() -> Node:
 	if randi() % 3 == 0:
 		return spore_bullet.instantiate()
 	return thorn_bullet.instantiate()
 
-func explode() -> void:
-	var offset = randf() * 2.0 * PI
-	for i in range(8):
-		var angle = offset + i * 2.0 * PI / 8.0
-		var dir = Vector2(cos(angle), sin(angle))
-		var bullet = spore_bullet.instantiate()
-		bullet.position = $BulletSpawnPoint.global_position + dir * 4.0
-		bullet.dir = dir
-		$/root/Main/Lawn.add_child(bullet)
-	queue_free()
-
 func shoot() -> void:
 	if player.health <= 0:
 		return
 	
 	var offset = randf() * 2.0 * PI
-	# Shoot 3 bullets at once
+	# Shoot 6 bullets at once
 	for i in range(0, 6):
 		var angle = offset + i * 2.0 * PI / 6.0
 		var bullet = rand_bullet()
 		bullet.global_position = $BulletSpawnPoint.global_position
-		bullet.speed = BULLET_SPEED
+		bullet.speed = bullet_speed
 		bullet.dir = Vector2(cos(angle), sin(angle))
 		$/root/Main/Lawn.add_child(bullet)
 
 func _process(delta: float) -> void:
-	$Healthbar.update_bar(health, MAX_HEALTH)
+	update(delta, $BulletSpawnPoint.global_position)
 	
-	if stun_timer > 0.0:
-		stun_timer -= delta
-		$StunParticles.emitting = true
-		$StunParticles.show()
-		$AnimatedSprite2D.animation = "stunned"
-	else:
-		$StunParticles.emitting = false
-		$StunParticles.hide()
-		$AnimatedSprite2D.animation = "default"
-	
-	if stun_timer <= 0.0:
-		shoot_timer -= delta
-	var dist = (player.get_sprite_pos() - $BulletSpawnPoint.global_position).length()
-	if dist < SHOOT_RANGE and shoot_timer <= 0.0 and stun_timer <= 0.0:
-		shoot()
-		shoot_timer = SHOOT_COOLDOWN
-	
-	lawnmower_damage_timer -= delta
-	if lawnmower_damage_timer <= 0.0 and inside_lawnmower:
-		health -= 1
-		health = max(health, 0)
-		lawnmower_damage_timer = LAWNMOWER_DAMAGE_COOLDOWN
-		stun()
-	
-	if health <= 0:
-		explode()
-
-func stun() -> void:
-	stun_timer += STUN_AMT
-	stun_timer = max(stun_timer, STUN_AMT)
+	if dead():
+		explode(spore_bullet, 8, $BulletSpawnPoint.global_position)
 
 func _on_area_entered(area: Area2D) -> void:
 	if area is PlayerBullet:
